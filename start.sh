b7f887835d5a15177d93fd06b5818109a05b41d8
JustPin <68527260+Kepin-ui@users.noreply.github.com>
2021-06-24 20:50:17 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -Xms1024M -Xmx1024M -jar Waterdog.jar
+java -Xms1024M -Xmx1024M -jar Waterdog.jar -Dio.netty.tryReflectionSetAccessible=true
+--add-opens java.base/jdk.internal.misc=ALL-UNNAMED
